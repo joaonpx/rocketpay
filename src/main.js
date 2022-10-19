b1217ed34599cff1d02aa4f2a2f400e5f9a22d32
Npx <cheyprivado@gmail.com>
2022-10-19 19:41:11 -0300

    :lipstick: Atualizando UI

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
 import './css/index.css'
 
-const ccBgColor01 = document.querySelector(
-  '.cc-bg svg > g g:nth-child(1) ellipse'
-)
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
-const ccBgColor03 = document.querySelector(
-  '.cc-bg svg > g g:nth-child(3) ellipse'
-)
 const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
 
 function setCardType(type) {
   const colors = {
-    visa: ['#4959EC', '#8000CE', '#5CCEFF'],
-    mastercard: ['#F16529', '#FF3C21', '#FFA724'],
-    default: ['none', 'none', 'none']
+    visa: ['#436D99', '#2D57F2'],
+    mastercard: ['#C69347', '#DF6F29'],
+    default: ['none', 'none']
   }
 
   ccBgColor01.setAttribute('fill', colors[type][0])
   ccBgColor02.setAttribute('fill', colors[type][1])
-  ccBgColor03.setAttribute('fill', colors[type][2])
   ccLogo.setAttribute('src', `cc-${type}.svg`)
 }
 
